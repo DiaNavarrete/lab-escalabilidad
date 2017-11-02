d4635b1b7becb2a4c07d6d11f042ab2bd6f8e792
Diana <dcnavarreter@unal.edu.co>
2017-11-02 08:52:46 -0500

    image

--- start2.sh
+++ start2.sh
@@ -1,4 +1,4 @@
-./rancher-compose --project-name contapp \
+./rancher-compose.exe --project-name contapp \
     --url http://192.168.99.101:8080/v1/projects/1a5 \
     --access-key E047D7DC0FE33FC30804 \
     --secret-key BaxWpYoB2QQEVSGNdoVr9miEzG7gFGovx3jFmRFd \
